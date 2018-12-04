906967276cdc4405e824bb8ae5e756e81575320e
andreybannikov <andrewnnov@yandex.ru>
2018-12-04 17:20:57 +0300

    final hw00

--- src/test/java/ru/javawebinar/topjava/UserMealsUtilTest.java
+++ src/test/java/ru/javawebinar/topjava/UserMealsUtilTest.java
@@ -8,7 +8,7 @@ import ru.javawebinar.topjava.util.UserMealsUtil;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -21,23 +21,16 @@ public class UserMealsUtilTest {
     public void whenWeHaveOnlyBreakfastThenOneNote() {
 
 
-        List<UserMeal> mealList = new ArrayList<>();
-        UserMeal userMeal1 = new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500);
-        UserMeal userMeal2 = new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000);
-        UserMeal userMeal3 = new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500);
+        List<UserMeal> mealList = Arrays.asList(
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+        );
 
-        UserMeal userMeal4 = new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000);
-        UserMeal userMeal5 = new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500);
-        UserMeal userMeal6 = new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510);
-
-        mealList.add(userMeal1);
-        mealList.add(userMeal2);
-        mealList.add(userMeal3);
-        mealList.add(userMeal4);
-        mealList.add(userMeal5);
-        mealList.add(userMeal6);
-
-        List<UserMealWithExceed> result = userMealsUtil.getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        List<UserMealWithExceed> result = userMealsUtil.getFilteredWithExceededStream(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
 
 
 
